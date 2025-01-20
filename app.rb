c81a846a183b4b94f1daf08adc261de6d5360355
EirikNilsson <eirik.haugennilsson@elev.ga.ntig.se>
2025-01-20 15:02:48 +0100

    Startcode + log in

--- app.rb
+++ app.rb
@@ -1,7 +1,91 @@
+require 'sinatra'
+require 'sqlite3'
+require 'bcrypt'
+
 class App < Sinatra::Base
+  def db_connection
+    db = SQLite3::Database.new 'db/users.sqlite'
+    db.results_as_hash = true
+    db
+  end
 
-    get '/' do
-        erb(:"index")
+  configure do
+    enable :sessions
+    set :session_secret, SecureRandom.hex(64)
+  end
+
+  get '/' do
+    if session[:user_id]
+      erb(:"training")
+    else
+      erb :index
     end
+  end
 
+  get '/signup' do
+    erb :newuser 
+  end
+
+
+  post '/signup' do
+    username = params[:username]
+    plain_password = params[:password]
+    password_hashed = BCrypt::Password.create(plain_password)
+    db = db_connection
+    db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [username, password_hashed])
+    redirect '/' 
+  end
+
+  post '/testpwcreate' do
+    plain_password = params[:plainpassword]
+    password_hashed = BCrypt::Password.create(plain_password)
+    p password_hashed
+  end
+
+  get '/admin' do
+    if session[:user_id]
+      erb(:"admin/index")
+    else
+      status 401
+      redirect '/unauthorized'
+    end
+  end
+
+  get '/unauthorized' do
+    erb(:unauthorized)
+  end
+
+  post '/login' do
+    request_username = params[:username]
+    request_plain_password = params[:password]
+    db = db_connection
+    user = db.execute("SELECT * FROM users WHERE username = ?", request_username).first
+    unless user
+      status 401
+      redirect '/unauthorized'
+    end
+    db_id = user["id"].to_i
+    db_password_hashed = user["password"].to_s
+    bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+    if bcrypt_db_password == request_plain_password
+      session[:user_id] = db_id
+      redirect '/training'
+    else
+      status 401
+      redirect '/unauthorized'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+  get '/training' do
+    erb(:training)
+  end
+
+  get '/diet' do
+    erb(:diet)
+  end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,39 @@
+
 require 'sqlite3'
+require 'bcrypt'
 
 class Seeder
-
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE users (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  username TEXT UNIQUE NOT NULL,
+                  password TEXT NOT NULL
+                )')
+  end
+
+  def self.populate_tables
+    password_hashed = BCrypt::Password.create("189")
+    db.execute('INSERT INTO users (username, password) VALUES (?, ?)', 
+               ["eirik", password_hashed])
+  end
+
+  private
+
+  def self.db
+    @db ||= SQLite3::Database.new('db/users.sqlite').tap do |db|
+      db.results_as_hash = true
+    end
+  end
 end
+
+Seeder.seed!
